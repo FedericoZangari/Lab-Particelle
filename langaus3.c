da5023840ba7cdcf3091b5b62c9373e00e58ce4d
Federico <federico.zangari@studenti.unimi.it>
2023-11-27 11:46:28 +0100

    dati misura 1

--- langaus3.c
+++ langaus3.c
@@ -1,27 +1,53 @@
 using namespace RooFit;
 
+TH1F* ReadFillAll( const char* Filename) {
+    
+	ifstream f(Filename);
+  TH1F *h1 = new TH1F("h1", "Spettro", 16383, 0, 16383);
+  int i = 0;
+	if(!f){
+  		cerr <<"Error: cannot open file " <<endl;
+		exit(0);
+	}
+  else{
+      double var;
+			for (;;){
+					
+          f >> var;
+          h1->SetBinContent(i,var);		
+									
+					if(f.eof()){
+						cout << "End of file reached "<< endl;
+						break;
+					}
+          i++;		
+			}
+		}	f.close();
+
+	return h1;
+}
+
 void langaus3(){
     // Creazione di un istogramma con dati di esempio
-    TH1D *histogram = new TH1D("histogram", "Landau convoluta con Gaussiana", 100, -50, 200);
-    for (int i = 0; i < 100000; ++i) {
-        double value = 35.0 + gRandom->Landau(0, 10) + gRandom->Gaus(0, 5);
-        histogram->Fill(value);
-    }
+    TH1F *histogram = ReadFillAll("dati_landau1.dat");
+    
 
     // Creazione delle variabili RooFit
-    RooRealVar x("x", "x", -50, 200);
+    RooRealVar x("x", "x", 80, 180);
     RooDataHist data("data", "data", x, Import(*histogram));
 
     // Creazione dei parametri per la funzione di Landau
-    RooRealVar mean("mean", "mean", 1, 0, 500);
+    RooRealVar mean("mean", "mean", 30, 0, 500);
     RooRealVar sigma("sigma", "sigma", 1, 0, 200);
     RooLandau landau("landau", "landau", x, mean, sigma);
 
     // Creazione dei parametri per la funzione Gaussiana
-    RooRealVar gauss_mean("gauss_mean", "gauss_mean", 10, -100, 100);
+    RooRealVar gauss_mean("gauss_mean", "gauss_mean", 1, -100, 100);
     RooRealVar gauss_sigma("gauss_sigma", "gauss_sigma", 22, 0, 100);
     RooGaussian gauss("gauss", "gauss", x, gauss_mean, gauss_sigma);
 
+    // Set #bins to be used for FFT sampling                                                                                                                                                                                                                                              
+    //t.setBins(5000,"cache"); ????????
     // Creazione della PDF convoluta
     RooFFTConvPdf model("model", "model", x, landau, gauss);
 
@@ -35,6 +61,7 @@ void langaus3(){
 
     // Creazione di un canvas e visualizzazione del risultato
     TCanvas *canvas = new TCanvas("canvas", "Fit Result", 800, 600);
+    frame->GetXaxis()->SetRangeUser(0,180);
     frame->Draw();
-    canvas->SaveAs("langaus3_test.png");
+    canvas->SaveAs("dati_landau1.png");
 } 
